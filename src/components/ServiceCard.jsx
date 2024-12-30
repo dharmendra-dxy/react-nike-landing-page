acb3664a87c37f3285e7fb138b1e19ff0ddfa3cf
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2024-12-30 12:31:55 +0530

    changes

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Servicecard = ({imgURL, label, subtext}) => {
+const ServiceCard = ({imgURL, label, subtext}) => {
   return (
     <div 
     className='flex-1 sm:w-[350px] sm-min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16'
@@ -27,4 +27,4 @@ const Servicecard = ({imgURL, label, subtext}) => {
   )
 }
 
-export default Servicecard
+export default ServiceCard
